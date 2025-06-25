a5591b665babfbb3c2f0bcc0641d561b18a1d1db
GUILHERME NUNES <gui.nunes1983@gmail.com>
2025-06-25 22:23:16 +0000

    Concluido o JavaScript

--- Script.js
+++ Script.js
@@ -1,12 +1,19 @@
 /*Lista de ações do usuario
-    [] Pegar texto do input
-    [] Pegar data do input
-    [] Adicionar nova tarefa
-    [] Deletar tarefa  
+    [OK] Pegar texto do input
+    [x] Pegar data do input
+    [ok] Adicionar nova tarefa
+    [ok] Deletar tarefa  
 */
 
 function adicionarTarefa() {
-    let input = document.querySelector("input").value
+    let valorInput = document.querySelector("input").value;
+    let li = document.createElement("li");
+    li.classList.add("tarefas");
+    li.innerHTML = valorInput + '<span onclick="deletarTarefa(this)">❌</span>';
+    document.querySelector("#listaTarefas").appendChild(li);
+    document.querySelector("input").value = ""; // Limpa o input após adicionar a tarefa
+}
 
-    console.log(input)
+function deletarTarefa(li) {
+    li.parentElement.remove(li);
 }

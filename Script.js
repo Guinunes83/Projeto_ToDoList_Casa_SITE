ff39adda14fc305300dc5d6441d08f59a92ee365
GUILHERME NUNES <gui.nunes1983@gmail.com>
2025-06-25 04:16:23 +0000

    criando JavaScript

---
+++ Script.js
@@ -0,0 +1,12 @@
+/*Lista de ações do usuario
+    [] Pegar texto do input
+    [] Pegar data do input
+    [] Adicionar nova tarefa
+    [] Deletar tarefa  
+*/
+
+function adicionarTarefa() {
+    let input = document.querySelector("input").value
+
+    console.log(input)
+}
